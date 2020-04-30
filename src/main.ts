509146cc1e36e8f8012ba453404795c5e3580731
Kelvin <kelvin@192.168.0.103>
2020-04-30 22:15:11 +0800

    add jest module

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import './style/main.scss';
+
+const hi = (): number => {
+    console.log("hi");
+    return 1
+};
+
+export default hi;
---
+++ test/main.test.ts
@@ -0,0 +1,5 @@
+import hi from '../src/main';
+
+test('test hi function', () => {
+    expect(hi()).toBe(1);
+})
